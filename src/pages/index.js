0c5585fc20b45d3ee05db5326a6010ac77223f8a
andersons-tuche23 <134718682+andersons-tuche23@users.noreply.github.com>
2023-08-10 14:55:20 -0300

    props-componetizacao

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,67 +1,20 @@
-const boolean = true
-const string = "felipe"
-const number = 10
-const float = 10.1
-const sem_resultado = undefined
-const nula = null
-const array = []//lista
-const objeto = {}
+import React from 'react';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Secao1 from '@/components/Secao1';
+import Secao2 from '@/components/Secao2';
 
-const celula = [
-  {
-    lider: "fulano",
-    numero_celula: 10,
-    mais_de_10: true,
-    membros: ["abreu"]
-  },
-  {
-    lider: "fulano 2",
-    numero_celula: 11,
-    mais_de_10: false,
-    membros: ["joao", "maria", "zezinho"]
-  }
-]
+function pages() {
 
+const mostrar = true
+  return (
+    <div className="App">
+      <Header/>
+      <Secao1 />
+      <Secao2/>
+      <Footer mostrar={mostrar}/>
+    </div>
+  );
+}
 
-//exercicio
-
-// 1-lista, 3-objetos, 7-dados : 3-string, 2-bool, 1 number, 1dado(objeto, 2 dados)
-
- 
-const atleta [
-  {
-    atleta: "michael_jorda ",
-    nascido: "17/02/1963",
-    drafitado: "12/07/1984",
-    titulos: 6,
-    aposentou:true,
-    falecido: false,
-    equipes:[1"chicago bulls",2" washington wizards"] 
-  },
-  {
-    atleta: "stephen curry",
-    nascido: "14/03/1988",
-    drafitado: "25/06/2009",
-    titulos: 4,
-    aposentou:false,
-    falecido: false,
-    equipes:[1"golden state warriors",2"davidson wildcats"]
-  },
-  {
-    atleta: "lebron james",
-    nascido: "30/12/1984",
-    drafitado: "26/06/2003",
-    titulos:4,
-    aposentou:false,
-    falecido: false,
-    equipes:[1"miami heat",2"los angeles lakers"]
-  },
-]
-
-
-
-
-
-
-
-
+export default pages;
