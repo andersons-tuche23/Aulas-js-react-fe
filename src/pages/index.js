f139b91cd34e835003772e634ce9e2bbbb183313
andersons-tuche23 <134718682+andersons-tuche23@users.noreply.github.com>
2023-08-14 20:43:13 -0300

    exercicio-map1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import Secao from '@/components/Secao';
+// import './app.css';
+import styles from '../styles/Home.module.css';
 
-// vai fazer 4cards que usem o mesmo componente porem mostre texto diferentes
-// mostrar 2 links que cosomem o mesmo componente que vai ser passado um boleano pro props, se
-//eu passar true exibe o link, se eu passar false ou não passa nada, não exibe o link
+function app(){
 
-function pages() {
+    // o Map sempre é usado para trazer dados de um array []
+    
+    
+    const parentes = [
+        {
+            nome: "anderson",
+            data_de_nascimento:"07/07/1983",
+            idade:40,
+            telefone:8888-8888,
+            mais_de_1: true,
+            nome_da_mae: "carmem",
+            nome_do_pai:"jose",
+        },
+        {
+            nome: "giovanna",
+            data_de_nascimento:"12/09/2005",
+            idade:17,
+            telefone:5555-5555,
+            mais_de_1: true,
+            nome_da_mae:"graziele", 
+            nome_do_pai:"anderson",
+        },
+        {
+            nome: "pietro",
+            data_de_nascimento:"11/07/2019",
+            idade:4,
+            telefone:2222-2222,
+            mais_de_2: true,
+            nome_da_mae: "graziele",
+            nome_do_pai:"",
+        },
+        {
+            nome:"julia",
+            data_de_nascimento:"15/08/2017",
+            idade:6,
+            telefone:4444-4444,
+            mais_de_1: true,
+            nome_da_mae: "patricia",
+            nome_do_pai:"",
+        },
+        {
+            nome: "robson",
+            data_de_nascimento:"14/08/2022",
+            idade:1,
+            telefone:7777-7777,
+            mais_de_1: true,
+            nome_da_mae:"joelma",
+            nome_do_pai:"",
+        }
+    ] 
 
-const mostrar = true
-const textoFooter= "esse é o texto do footer"
-  return (
-    <div className="App">
-      <Header texto={"esse é o texto do heade"} image={mostrar}/>
-      <Secao texto={"esse é o texto da seção1"}/>
-      <Secao texto={"esse é o texto da seção2"}/>
-      <Footer texto={textoFooter}/>
-    </div>
-  );
+    //fazer uma lista exibindo 5 pessoas com nome, idade, data de nascimentto e nome da mãe, e para 3 pessoas não vai ter o nome do pai
+// o nome do pai, porem quem não tiver o pai nem o  titulo pai pode aparecer//
+    return(
+        <div className="app">
+            {
+            parentes.map(item =>{
+                return(
+                    <div style={{display:'flex', gap:'25px', justifyContent:'space-evenly'}}>
+                    <div>nome:{item.nome}</div>
+                    <div>data_de_nascimento:{item.data_de_nascimento}</div>
+                    <div>idade:{item.idade}</div>
+                    <div>telefone:{item.telefone}</div>
+                    <div>nome_da_mae:{item.nome_da_mae}</div>
+                     {item.nome_do_pai &&
+                    <div>nome_do_pai{item.nome_do_pai}</div>
+                     }
+                    </div>   
+                )
+            })
+}
+        </div>
+    );
 }
 
-export default pages;
+export default app;
