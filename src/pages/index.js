17b4443b48056739e82deeba07e31881555af0b0
andersons-tuche23 <134718682+andersons-tuche23@users.noreply.github.com>
2023-09-01 16:18:50 -0300

    exercicio query string 01-09-23

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,50 @@
 import React, { useEffect } from "react";
 import styles from '@/styles/Home.module.css';
 
+// primeira página coloca os 2 links mandando para duas paginas diferenres uma você vai
+// tratar a ccondição do split, e na outra você vai tratar a condição da query string
+
+//SPLIT
+//primeiro exercicio vc vai montar um link que tenha 3 opções de parametro, você vai
+//exibir um conteudo diferente para cada caso
+
+//QUERY STRING
+//segundo caso você vai montar um segundo link com 5 opções de parametro, voce vai exibir
+//um conteudo proprio, condizente com os 5 parametros
+
+  //htt://localhost:3000/?loja=nike&cor=preto&camiseta&cor=azul
+
+
+
 function Home() {
     useEffect(() => {
         const url = window.location.href;
-        
-        // Split the URL parameters
-        const splitLoja = url.split('loja')[1];
-        console.log(splitLoja, 'splitLoja');
-        
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []); // Empty dependency array means this effect runs once on component mount
-    
-    // Rendering text using JSX syntax
+
+
+        const splitNike = url.split('nike')[1];
+        console.log(splitNike, 'splitNike');
+
+
+      
+
+    }, []);
+
+
     return (
-        <div className={styles.texto}>
-            <>texto americanas</>
-        </div>
+        <>
+            <div className={styles.texto}>
+                <div className={styles.cores}>
+                    <p className={styles.back}></p>
+                    <p className={styles.color}></p>
+                </div>
+                <img className={styles.img} src="/nike.png" width={500} />
+
+            </div>
+            
+            <div>
+                <a href="https://www.pngwing.com/pt/free-png-hmvxc"></a>
+            </div>
+        </>
     );
 }
 
